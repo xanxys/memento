a6caad489daf2f7f03ee3bc2bf18b7327005dee2
xanxys <xanxys@gmail.com>
2019-05-12 21:56:33 +0900

    Make search faster by removing raw data from vue.js watching

--- memento.js
+++ memento.js
@@ -1,11 +1,62 @@
 
+/** Searchable on-memory immutable index of tweets. */
+class TweetIndex {
+    /**
+     * 
+     * @param {Array<Object>} tweets Objects from tweet.json in newest-first order.
+     */
+    constructor(tweets) {
+        this.tweets = tweets;
+    }
+
+    /**
+     * O(1)
+     */
+    getCount() {
+        return this.tweets.length;
+    }
+
+    search(query) {
+        return this.tweets.filter(tweet => {
+            if (query.length === 0) {
+                return true;
+            }
+            return tweet.full_text.indexOf(query) >= 0;
+        }).map(tweet => {
+            return {
+                text: tweet.full_text,
+                localYear: new Date(tweet.created_at).getFullYear(),
+            };
+        });
+    }
+}
+
+/**
+ * 
+ * @param {ArrayBuffer|File} stream twitter .zip expoted data
+ * @return {Promise<TweetIndex>}
+ */
+function createTweetIndexFromTwitterDump(stream) {
+    return JSZip.loadAsync(stream)
+        .then(zip => zip.file("tweet.js").async("string"))
+        .then(tweetJson => {
+            tweetJson = tweetJson.replace("window.YTD.tweet.part0 =", "");
+            const tweets = JSON.parse(tweetJson);
+            tweets.sort((a, b) => {
+                return new Date(b.created_at) - new Date(a.created_at);
+            });  // newest first
+            return new TweetIndex(tweets);
+        });
+}
+
 function main() {
     Vue.use(SemanticUIVue);
 
+    const maybeIndex = { index: null };
     var app = new Vue({
         el: '#app',
         data: {
-            tweets: [],
+            indexLoaded: false,
             searchQuery: "",
         },
         methods: {
@@ -13,42 +64,32 @@ function main() {
                 this.loadTwitterZipStream(event.srcElement.files[0]);
             },
             loadTwitterZipStream: function (stream) {
-                JSZip.loadAsync(stream).then(zip => {
-                    zip.file("tweet.js").async("string").then(tweetJson => {
-                        tweetJson = tweetJson.replace("window.YTD.tweet.part0 =", "");
-                        const tweets = JSON.parse(tweetJson);
-                        tweets.sort((a, b) => {
-                            return new Date(b.created_at) - new Date(a.created_at);
-                        });  // newest first
-                        this.tweets = tweets;
-                    });
+                createTweetIndexFromTwitterDump(stream).then(index => {
+                    maybeIndex.index = index;
+                    this.indexLoaded = true;
                 });
             },
         },
         computed: {
+            numTotal: function () {
+                if (!this.indexLoaded) {
+                    return 0;
+                }
+                return maybeIndex.index.getCount();
+            },
+            filteredTweets: function () {
+                if (!this.indexLoaded) {
+                    return [];
+                }
+                return maybeIndex.index.search(this.searchQuery);
+            },
             searchResults: function () {
-                console.log(this.tweets[0]);
-                return this.tweets.filter(tweet => {
-                    if (this.searchQuery.length === 0) {
-                        return true;
-                    }
-                    return tweet.full_text.indexOf(this.searchQuery) >= 0;
-                }).map(tweet => {
-                    return {
-                        text: tweet.full_text
-                    };
-                });
-
+                return this.filteredTweets.slice(0, 1000); // DOM performance optimization
             },
             years: function () {
-                const years = new Map(); // local year
-                this.tweets.filter(tweet => {
-                    if (this.searchQuery.length === 0) {
-                        return true;
-                    }
-                    return tweet.full_text.indexOf(this.searchQuery) >= 0;
-                }).forEach(tweet => {
-                    const year = new Date(tweet.created_at).getFullYear();
+                const years = new Map();
+                this.filteredTweets.forEach(tweet => {
+                    const year = tweet.localYear;
                     if (years.has(year)) {
                         years.set(year, years.get(year) + 1);
                     } else {
